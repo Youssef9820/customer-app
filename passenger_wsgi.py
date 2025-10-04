1b18b3153835e9a5a87624253d54cd20db3fe823
Youssef9820 <youibm202017@gmail.com>
2025-10-05 00:44:14 +0300

    Add deployment files for Hostinger

---
+++ passenger_wsgi.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+# This line points to your project's directory
+sys.path.insert(0, os.path.dirname(__file__))
+
+# This line imports your Flask app object from your app.py file
+from app import app as application
